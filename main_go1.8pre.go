c865193e69fc7fa6ba8ec1c94c7ae2fecdb31178
Reza Mohammadi <reza@cafebazaar.ir>
2017-04-08 21:02:53 +0430

    Utilize go1.8 context support in database/sql
    Fixes #1231
    
    The related go1.8 release notes: https://golang.org/doc/go1.8#database_sql

---
+++ main_go1.8pre.go
@@ -0,0 +1,17 @@
+// +build !go1.8
+
+package gorm
+
+// Begin starts a transaction
+func (s *DB) Begin() *DB {
+	c := s.clone()
+	if db, ok := c.db.(sqlDb); ok && db != nil {
+		tx, err := db.Begin()
+		c.db = interface{}(tx).(SQLCommon)
+		c.dialect.SetDB(c.db)
+		c.AddError(err)
+	} else {
+		c.AddError(ErrCantStartTransaction)
+	}
+	return c
+}
